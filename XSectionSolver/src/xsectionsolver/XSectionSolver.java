b5b39fd8bfffcdd078dd89d0dd6aafe61b621f00
xyz <zhupeter010903@163.com>
2019-06-06 19:09:26 -0400

    Merge origin/master
    Conflicts:
    	src/sister/Questions.txt

--- XSectionSolver/src/xsectionsolver/XSectionSolver.java
+++ XSectionSolver/src/xsectionsolver/XSectionSolver.java
@@ -1,6 +1,7 @@
 
 package xsectionsolver;
 
+import java.text.DecimalFormat;
 import org.mariuszgromada.math.mxparser.*;
 import org.mariuszgromada.math.mxparser.mathcollection.*;
 import org.mariuszgromada.math.mxparser.parsertokens.*;
@@ -17,11 +18,15 @@ public class XSectionSolver {
         mXparser.consolePrintln(eh.getExpressionString() + " = " + eh.calculate());
         
         
-        Function f1 = new Function("f(x) = x^4");
-        Function f2 = new Function("f(x) = x");
-        Expression e = new Expression("int("+f1.getFunctionExpressionString()+",x,-1,1)");
+        Function f1 = new Function("f(x) = x^2");
+        Function f2 = new Function("f","x^x","x");
+        Expression e = new Expression("int("+f1.getFunctionExpressionString()+"^2,x,-1,1)");
         mXparser.consolePrintln(f1.getFunctionExpressionString()+","+e.getExpressionString() + " = " + e.calculate());
         
+        mXparser.consolePrintln(f2.getFunctionExpressionString()+ " = " + f2.calculate(2));
+        
+        Calculator cal = new Calculator("x","-x",0,20,1,-1,20,0,false,false);
+        System.out.println(cal.calculateTheoraticalVolume());
     }
     
 }
