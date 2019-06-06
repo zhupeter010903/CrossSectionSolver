38aacf0eed37c86b09c0de3542b41c81073f97fe
xyz <zhupeter010903@163.com>
2019-06-06 19:09:06 -0400

    Merge origin/master
    Conflicts:
    	src/sister/Questions.txt

--- XSectionSolver/src/xsectionsolver/Calculator.java
+++ XSectionSolver/src/xsectionsolver/Calculator.java
@@ -1,6 +1,7 @@
 
 package xsectionsolver;
 
+import java.text.DecimalFormat;
 import java.util.Objects;
 import org.mariuszgromada.math.mxparser.*;
 import org.mariuszgromada.math.mxparser.mathcollection.*;
@@ -10,23 +11,39 @@ import org.mariuszgromada.math.mxparser.syntaxchecker.*;
 
 public class Calculator {
     
-    private int MIN_ACTUAL_LENGTH = 20;
+    final static private int MIN_ACTUAL_LENGTH = 20;
     
-    private String f;
-    private String g;
+    final static private int XSECTION_SQUARE=0;
+    final static private int XSECTION_CIRCLE=1;
+    final static private int XSECTION_SEMICIRCLE=2;
+    final static private int XSECTION_EQUILIBRIUM_TRIANGLE=3;
+    final static private int XSECTION_RIGHT_ISOSCELES_TRIANGLE=4;
+    
+    final static private int RIGHT_RIEMANNSUM=0;
+    final static private int LEFT_RIEMANNSUM=0;
+    final static private int MIDDLE_RIEMANNSUM=0;
+    
+    private String Function1Expression;
+    private String Function2Expression;
+    private Function Function1;
+    private Function Function2;
     private int xSectionType;
     private int layersNum;
     private int upperLimit;
     private int lowerLimit;
     private int actualLength;
+    private int riemannSumType;
     private boolean fPieceWise;
     private boolean gPieceWise;
     
+    private DecimalFormat df = new DecimalFormat("0.######");
     
-    public Calculator(String f, String g, int xSectionType, int layersNum, int upperLimit, int lowerLimit, 
-            int actualLength, int riemannSumType, boolean fPieceWise, boolean gPieceWise) {
-        this.f = f;
-        this.g = g;
+    public Calculator(String Function1Expression, String Function2Expression, int xSectionType, int layersNum, 
+            int upperLimit, int lowerLimit, int actualLength, int riemannSumType, 
+            boolean fPieceWise, boolean gPieceWise) {
+        
+        this.Function1Expression = Function1Expression;
+        this.Function2Expression = Function2Expression;
         this.xSectionType = xSectionType;
         this.layersNum = layersNum;
         this.upperLimit = upperLimit;
@@ -35,15 +52,96 @@ public class Calculator {
         this.riemannSumType = riemannSumType;
         this.fPieceWise = fPieceWise;
         this.gPieceWise = gPieceWise;
+        
+        if(fPieceWise){
+            Function1 = new PieceWiseFunction("f",Function1Expression,"x");
+        }
+        else{
+            Function1 = new Function("f",Function1Expression,"x");
+        }
+        Function2 = new Function("f",Function2Expression,"x");
+    }
+    
+    public double calculateTheoraticalVolume(){
+        Expression volume = new Expression("int((("+Function1.getFunctionExpressionString()
+                +")-("+Function2.getFunctionExpressionString()+"))^2,x,"+lowerLimit+","+upperLimit+")");
+        return volume.calculate();
     }
 
+    public int getRiemannSumType() {
+        return riemannSumType;
+    }
 
+    public void setRiemannSumType(int riemannSumType) {
+        this.riemannSumType = riemannSumType;
+    }
+
+    public int getMIN_ACTUAL_LENGTH() {
+        return MIN_ACTUAL_LENGTH;
+    }
+
+    public String getFunction1Expression() {
+        return Function1Expression;
+    }
+
+    public void setFunction1Expression(String Function1Expression) {
+        this.Function1Expression = Function1Expression;
+    }
+
+    public String getFunction2Expression() {
+        return Function2Expression;
+    }
+
+    public void setFunction2Expression(String Function2Expression) {
+        this.Function2Expression = Function2Expression;
+    }
+
+    public int getxSectionType() {
+        return xSectionType;
+    }
+
+    public void setxSectionType(int xSectionType) {
+        this.xSectionType = xSectionType;
+    }
+
+    public int getLayersNum() {
+        return layersNum;
+    }
+
+    public void setLayersNum(int layersNum) {
+        this.layersNum = layersNum;
+    }
+
+    public int getUpperLimit() {
+        return upperLimit;
+    }
+
+    public void setUpperLimit(int upperLimit) {
+        this.upperLimit = upperLimit;
+    }
+
+    public int getLowerLimit() {
+        return lowerLimit;
+    }
+
+    public void setLowerLimit(int lowerLimit) {
+        this.lowerLimit = lowerLimit;
+    }
+
+    public int getActualLength() {
+        return actualLength;
+    }
+
+    public void setActualLength(int actualLength) {
+        this.actualLength = actualLength;
+    }
+    
     @Override
     public int hashCode() {
         int hash = 7;
         hash = 13 * hash + this.MIN_ACTUAL_LENGTH;
-        hash = 13 * hash + Objects.hashCode(this.f);
-        hash = 13 * hash + Objects.hashCode(this.g);
+        hash = 13 * hash + Objects.hashCode(this.Function1Expression);
+        hash = 13 * hash + Objects.hashCode(this.Function2Expression);
         hash = 13 * hash + this.xSectionType;
         hash = 13 * hash + this.layersNum;
         hash = 13 * hash + this.upperLimit;
@@ -86,10 +184,10 @@ public class Calculator {
         if (this.riemannSumType != other.riemannSumType) {
             return false;
         }
-        if (!Objects.equals(this.f, other.f)) {
+        if (!Objects.equals(this.Function1Expression, other.Function1Expression)) {
             return false;
         }
-        if (!Objects.equals(this.g, other.g)) {
+        if (!Objects.equals(this.Function2Expression, other.Function2Expression)) {
             return false;
         }
         return true;
@@ -97,80 +195,7 @@ public class Calculator {
 
     @Override
     public String toString() {
-        return "Calculator{" + "MIN_ACTUAL_LENGTH=" + MIN_ACTUAL_LENGTH + ", f=" + f + ", g=" + g + ", xSectionType=" + xSectionType + ", layersNum=" + layersNum + ", upperLimit=" + upperLimit + ", lowerLimit=" + lowerLimit + ", actualLength=" + actualLength + ", riemannSumType=" + riemannSumType + '}';
-    }
-    private int riemannSumType;
-
-    public int getRiemannSumType() {
-        return riemannSumType;
-    }
-
-    public void setRiemannSumType(int riemannSumType) {
-        this.riemannSumType = riemannSumType;
-    }
-
-    public int getMIN_ACTUAL_LENGTH() {
-        return MIN_ACTUAL_LENGTH;
-    }
-
-    public void setMIN_ACTUAL_LENGTH(int MIN_ACTUAL_LENGTH) {
-        this.MIN_ACTUAL_LENGTH = MIN_ACTUAL_LENGTH;
-    }
-
-    public String getF() {
-        return f;
-    }
-
-    public void setF(String f) {
-        this.f = f;
-    }
-
-    public String getG() {
-        return g;
-    }
-
-    public void setG(String g) {
-        this.g = g;
-    }
-
-    public int getxSectionType() {
-        return xSectionType;
-    }
-
-    public void setxSectionType(int xSectionType) {
-        this.xSectionType = xSectionType;
-    }
-
-    public int getLayersNum() {
-        return layersNum;
-    }
-
-    public void setLayersNum(int layersNum) {
-        this.layersNum = layersNum;
-    }
-
-    public int getUpperLimit() {
-        return upperLimit;
-    }
-
-    public void setUpperLimit(int upperLimit) {
-        this.upperLimit = upperLimit;
-    }
-
-    public int getLowerLimit() {
-        return lowerLimit;
-    }
-
-    public void setLowerLimit(int lowerLimit) {
-        this.lowerLimit = lowerLimit;
-    }
-
-    public int getActualLength() {
-        return actualLength;
-    }
-
-    public void setActualLength(int actualLength) {
-        this.actualLength = actualLength;
+        return "Calculator{" + "MIN_ACTUAL_LENGTH=" + MIN_ACTUAL_LENGTH + ", f=" + Function1Expression + ", g=" + Function2Expression + ", xSectionType=" + xSectionType + ", layersNum=" + layersNum + ", upperLimit=" + upperLimit + ", lowerLimit=" + lowerLimit + ", actualLength=" + actualLength + ", riemannSumType=" + riemannSumType + '}';
     }
     
 }
