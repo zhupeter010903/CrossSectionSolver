df307aaa333a00dfabbe396c4bd6c102786b974d
xyz <zhupeter010903@163.com>
2019-06-06 10:58:23 -0400

    Merge origin/master
    Conflicts:
    	src/sister/Questions.txt

--- XSectionSolver/src/org/mariuszgromada/math/mxparser/PieceWiseFunction.java
+++ XSectionSolver/src/org/mariuszgromada/math/mxparser/PieceWiseFunction.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public class PieceWiseFunction extends Function{
     private String functionName;
     private int pieceWiseNumber;
-    private double[][] pieceWiseLimits;
+    private Argument[][] pieceWiseLimits;
 
     public PieceWiseFunction(String functionDefinitionString, PrimitiveElement... elements) {
         super(functionDefinitionString, elements);
@@ -28,12 +28,12 @@ public class PieceWiseFunction extends Function{
         this.functionName = functionName;
         String functions[] = functionDefinitionString.split(",");
         pieceWiseNumber = functions.length/3;
-        pieceWiseLimits = new double [pieceWiseNumber][2];
+        pieceWiseLimits = new Argument [pieceWiseNumber][2];
         
         for(int i=0;i<pieceWiseNumber;i++){
             this.defineFunction(functionName, functions[3*i].trim(),ArgumentName);
-            pieceWiseLimits[i][0] = Double.parseDouble(functions[3*i+1].trim());
-            pieceWiseLimits[i][1] = Double.parseDouble(functions[3*i+2].trim());
+            pieceWiseLimits[i][0] = new Argument(ArgumentName+"="+functions[3*i+1].trim());
+            pieceWiseLimits[i][1] = new Argument(ArgumentName+"="+functions[3*i+2].trim());
         }
         
     }
@@ -43,12 +43,12 @@ public class PieceWiseFunction extends Function{
         String FunctionExpressionString=functionName+" = \n{\n";
         FunctionExpressionString += "   " +
                 this.getFunction(0).getFunctionExpressionString()
-                +", ["+pieceWiseLimits[0][0]+","+pieceWiseLimits[0][1]+"]\n";
+                +", ["+pieceWiseLimits[0][0].getArgumentValue()+","+pieceWiseLimits[0][1].getArgumentValue()+"]\n";
         
         for(int i=1;i<pieceWiseNumber;i++){
             FunctionExpressionString += "   " +
                     this.getFunction(i).getFunctionExpressionString()
-                    +", ["+pieceWiseLimits[i][0]+","+pieceWiseLimits[i][1]+")\n";
+                    +", ("+pieceWiseLimits[i][0].getArgumentValue()+","+pieceWiseLimits[i][1].getArgumentValue()+"]\n";
         }
         FunctionExpressionString += "}\n";
         return FunctionExpressionString;
@@ -56,15 +56,13 @@ public class PieceWiseFunction extends Function{
     
     public double calculate(Argument a){
         for(int i=0;i<pieceWiseNumber;i++){
-            System.out.println(pieceWiseLimits[i][0]+", "+a.getArgumentExpressionString()+", "+pieceWiseLimits[i][1]);
-            if (a.getArgumentValue()>=pieceWiseLimits[i][0]&&a.getArgumentValue()<=pieceWiseLimits[i][1]){
-                System.out.println("!");
+            
+            if (a.getArgumentValue()>=pieceWiseLimits[i][0].getArgumentValue()
+                    && a.getArgumentValue()<=pieceWiseLimits[i][1].getArgumentValue()){
                 return this.getFunction(i).calculate(a);
             }
         }
-        return 0.111;
+        return Double.NaN;
     }
-
-    
     
 }
